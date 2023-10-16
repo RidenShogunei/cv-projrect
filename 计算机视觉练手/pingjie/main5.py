eb75d69b0cd18a287c33d1a8a4640bc2e0142e81
陈瑾旭 <108864288+RidenShogunei@users.noreply.github.com>
2023-10-16 21:46:21 +0800

    10/16
    10/16

---
+++ 计算机视觉练手/pingjie/main5.py
@@ -0,0 +1,17 @@
+from Stitcher import Stitcher
+import cv2
+
+imageA = cv2.imread("left_01.png")
+imageB = cv2.imread("right_01.png")
+
+# 把图片拼接成全景图
+stitcher = Stitcher()
+(result, vis) = stitcher.stitch([imageA, imageB], showMatches=True)
+
+# 显示所有图片
+cv2.imshow("Image A", imageA)
+cv2.imshow("Image B", imageB)
+cv2.imshow("Keypoint Matches", vis)
+cv2.imshow("Result", result)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
